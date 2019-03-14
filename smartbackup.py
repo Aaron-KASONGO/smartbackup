aa39e5c8eeada1b69f6545ec938abe8ec8e7f34e
SimplyNate <horiuchi.nathan@gmail.com>
2019-03-13 18:45:55 -1000

    Added UI elements to GUI side of program

--- smartbackup.py
+++ smartbackup.py
@@ -26,9 +26,10 @@ import hashlib
 import os
 import shutil
 import datetime
-import sys
+import threading
 import platform
-import tkinter as tk
+import tkinter
+from tkinter import *
 from pathlib import Path
 
 
@@ -64,10 +65,60 @@ class Gui:
     verbosity = 1
 
     def __init__(self, master):
+        self.master = master
         master.title("Smart Backup")
-        master.geometry("640x480")
+        # master.geometry("640x480")
         master.resizable(True, True)
         master.configure(background='white')
+        # Interface variables
+        self.heading = Label(master, text="Smart Backup", background="white")
+        self.sourceLabel = Label(master, text="Source:", background="white")
+        self.source = Entry(master)
+        self.destinationLabel = Label(master, text="Destination:", background="white")
+        self.destination = Entry(master)
+        self.optionLabel = Label(master, text="Options", background="white")
+        # Runtime Options
+        self.vrs = []
+        self.av = IntVar()
+        self.qv = IntVar()
+        self.vv = IntVar()
+        self.a = Checkbutton(master, text="Copy ALL files", variable=self.av, background="white")
+        self.q = Checkbutton(master, text="Run Quietly", variable=self.qv, background="white")
+        self.v = Checkbutton(master, text="Run Verbosely", variable=self.vv, background="white")
+        self.logLabel = Label(master, text="Log to File:", background="white")
+        self.log = Entry(master)
+        self.hashtypeLabel = Label(master, text="Specify Hash Type:", background="white")
+        self.hashtype = Entry(master)
+        self.beginningLabel = Label(master, text="Specify Beginning Date:", background="white")
+        self.beginning = Entry(master)
+
+        # Start button
+        self.startButton = Button(master, text="Start", command=self.newthread)
+
+        # Place widgets on grid
+        self.heading.grid(row=0, column=0, columnspan=2)
+        self.sourceLabel.grid(row=2, column=0, sticky=W)
+        self.source.grid(row=3, column=0, columnspan=2, sticky=W+E)
+        self.destinationLabel.grid(row=5, column=0, sticky=W)
+        self.destination.grid(row=6, column=0, columnspan=2, sticky=W+E)
+        self.optionLabel.grid(row=8, column=0, columnspan=2)
+        self.a.grid(row=9, column=0)
+        self.q.grid(row=9, column=1)
+        self.v.grid(row=10, column=0)
+        self.logLabel.grid(row=11, column=0, sticky=W)
+        self.log.grid(row=12, column=0, columnspan=2, sticky=W+E)
+        self.hashtypeLabel.grid(row=13, column=0, sticky=W)
+        self.hashtype.grid(row=14, column=0, columnspan=2, sticky=W+E)
+        self.beginningLabel.grid(row=15, column=0, sticky=W)
+        self.beginning.grid(row=16, column=0, columnspan=2, sticky=W+E)
+        self.startButton.grid(row=18, column=0, columnspan=2)
+
+    def newthread(self):
+        t = threading.Thread(name='child procs', target=self.start)
+        t.start()
+
+    def start(self):
+        pass
 
 
 class Cli:
@@ -371,7 +422,7 @@ platf = platform.system()
 if __name__ == '__main__':
     # If the program is run with no arguments, run as GUI application
     if len(sys.argv) == 1:
-        root = tk.Tk()
+        root = tkinter.Tk()
         gui = Gui(root)
         root.mainloop()
     # If there are arguments provided, run as CLI program
